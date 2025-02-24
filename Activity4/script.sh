d8a7b93017d4d983d30de51443014ab3c19fa32d
French <130640700+frenchMontajes@users.noreply.github.com>
2025-02-24 06:20:54 +0000

    adad

--- Activity4/script.sh
+++ Activity4/script.sh
@@ -3,6 +3,16 @@
 echo "Enter your exam score [0-100]: " 
 read exam_score
 
-if [ $exam_score -gt 90 ]; then
-    echo "You got an A!"
-elif
+if [ $exam_score -gt 90 ] && [ $exam_score -le 100 ];  then
+    echo "Grade A"
+elif [ $exam_score -gt 80 ] && [ $exam_score -lt 90 ]; then
+    echo "Grade B"
+elif [ $exam_score -ge 70 ] && [ $exam_score -lt 80 ]; then 
+    echo "Grade C"
+elif [ $exam_score -ge 60 ] && [ $exam_score -lt 70 ]; then
+    echo "Grade D"
+elif [ $exam_score -lt 60 ] && [ $exam_score -ge 0 ]; then
+    echo "Grade F"
+else 
+    echo "ERROR: Invalid input!"
+fi 
