9fa7fcfdb6a34b8793d59089b30331cbf6f4938d
French <130640700+frenchMontajes@users.noreply.github.com>
2025-04-07 06:21:05 +0000

    update

--- Activity6/log_time.sh
+++ Activity6/log_time.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Current date and time: $(date)" >> Activity6/log.txt
+echo "Log updated by $(GITHUB_ACTOR) at $(date)" >> Activity6/log.txt
+
+log_count=$(wc -l < Activity6/log.txt)
+
+echo "The log has been updated $(log_count)) times." >> Activity6/log.txt
